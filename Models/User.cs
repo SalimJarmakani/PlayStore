3919c4409192547b5b84ecfe931e8d4e57ec5e09
IbrahimBayyari <98158831+IbrahimBayyari@users.noreply.github.com>
2023-03-25 15:33:18 +0200

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,16 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace PlayStore.Models
 {
     public class User
     {
-
         public int Id { get; set; }
-        public string? Username { get; set; }
-        public string? Password { get; set; }
-        public string? Profile_pic { get; set; }
-        public ICollection<Review>? LastViewed { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Profile_pic { get; set; }
+        public ICollection<Item> LastViewed { get; set; }
         public bool Recv_emails { get; set; }
 
+        public ICollection<Item> GetLatestTwentyFour()
+        {
+            var itemsViewedList = LastViewed.ToList();
+            int count = itemsViewedList.Count;
+            if (count > 24)
+            {
+                itemsViewedList.RemoveRange(0, count - 24);
+            }
+            return itemsViewedList;
+        }
     }
 }
